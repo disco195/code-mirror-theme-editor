ca78fa3612a4285d804f5d338e73942e59b7f0ca
mwaizman <waizman.meir@gmail.com>
2021-02-26 16:38:22 -0800

    Initial commit

---
+++ src/Topbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export class Topbar extends React.Component {
+	render = () => (
+		<header className="Topbar">
+			<p className="logo">CodeMirror Theme Editor</p>
+		</header>
+	)
+}
